f27dac1a1808020cbaab93e1ba57b2ea4ea1eb8f
brownm26 <empoleon207@gmail.com>
2020-09-14 13:26:41 -0600

    Added dark theme

--- app/src/main/java/com/mbrown/myapplication/App.kt
+++ app/src/main/java/com/mbrown/myapplication/App.kt
@@ -1,11 +1,13 @@
 package com.mbrown.myapplication
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import timber.log.Timber
 
 class App : Application() {
 
     override fun onCreate() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         super.onCreate()
         Timber.plant(Timber.DebugTree())
     }
